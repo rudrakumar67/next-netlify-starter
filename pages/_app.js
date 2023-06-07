bc5cd2a3347cd9662ed760360465721421c789a5
Shivkumar <acharyashivkumar67@gmail.com>
2023-06-07 15:54:31 +0530

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
 import '@styles/globals.css'
+import Script from "next/script";
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id="google-analytics-script" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+          });
+    `}
+      </Script>
+      <Component {...pageProps} />
+    </>)
 }
 
 export default Application
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import Script from 'next/script';
 
 export default function Home() {
   return (
@@ -9,18 +8,6 @@ export default function Home() {
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
-        <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-SLPE8JTEJ2`} />
-
-        <Script strategy="lazyOnload">
-          {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', 'G-SLPE8JTEJ2', {
-                    page_path: window.location.pathname,
-                    });
-                `}
-        </Script>
       </Head>
 
       <main>
